3d157d256c770ee52f904a75645b43f02a62e898
Shane Phillips <shanekphillips@gmail.com>
2018-02-16 20:45:34 -0700

    initial commit

---
+++ multisitestataggregator.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Multisite Stat Aggregator
+ * Plugin URI:      https://xwp.co/
+ * Description:     A frontend widget that shows stats about a site (number of posts, users, etc) and the other sites on a multisite network; the widget is “live”, the stats are updated in the widget once a minute via Ajax communicating over the REST API to custom endpoints for stats.
+ * Author:          Shane Phillips
+ * Author URI:      shanephillips.tech
+ * Text Domain:     multisitestataggregator
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Multisitestataggregator
+ */
+
+
---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Multisite Stat Aggregator ===
+Contributors: siphon
+Tags: posts, ajax, widget
+Requires at least: 4.4
+Tested up to: 4.9.4
+Stable tag: 0.1.0
+License: GPLv2 or later
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+
