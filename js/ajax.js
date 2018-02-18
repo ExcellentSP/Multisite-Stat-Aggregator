1b98b9c7dbc118299106117400f5bc6c1f6b6f76
Shane Phillips <shanekphillips@gmail.com>
2018-02-18 16:46:49 -0700

    enable ajax

---
+++ js/ajax.js
@@ -0,0 +1,20 @@
+(function($){
+
+    var ajaxRequest = function(){
+        var siteID = $( '#mssa_select_site' ).val();
+        $.ajax({
+            url: "/wp-json/vendor/mssa/v1/site/" + siteID,
+            context: document.body,
+            accepts: 'application/json'
+        }).done(function( response ) {
+            response = JSON.parse(response);
+            var mss = $( "#mssa_multisite_stats" );
+            mss.empty();
+            mss.append( response.body );
+        });
+    };
+
+    var siteSwitch = $( '#mssa_select_site' );
+    siteSwitch.on( 'change', ajaxRequest );
+
+})(jQuery);
